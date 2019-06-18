d97874168dc401bead679f8feaa6992c8c0f4beb
fredlc <fredlefevre@outlook.com>
2019-06-18 19:27:32 -0400

    Initial commit

---
+++ iOSWeatherApp/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSWeatherApp
+//
+//  Created by Fred Lefevre on 2019-06-18.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
